d56e216fd1a84f1c9aa7b18c8e79182191015976
KevinFRX <xerokf13@gmail.com>
2023-07-10 15:42:46 -0300

    ready for deployment

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,14 +5,21 @@ import './Todo.css'
 
 export default function Todo({ className, id, text, isDone, onDelete }) {
     const [showConfirmDelete, setShowConfirmDelete] = useState(false)
+    const [isChecked, setIsChecked] = useState(isDone)
 
     return (
-        <div className={`${className} todo-container`}>
+        <div className={`${className} todo-container ${isChecked ? "done" : ""}`}>
             <ModalDialog title="Delete Task" isOpened={showConfirmDelete} onProceed={onDelete} onClose={() => setShowConfirmDelete(false)} acceptLabel="Accept" cancelLabel="Cancel">
                 <p>Are you sure you want to delete this task?</p>
             </ModalDialog>
-            <input type="checkbox" name="todo-checkbox" />
-            <div>{text}</div>
+            <input
+                className="checkbox"
+                type="checkbox"
+                name="todo-checkbox"
+                checked={isChecked}
+                onChange={(e) => setIsChecked(e.target.checked)}
+            />
+            <div className={`todo-text ${isChecked ? "done" : ""}`}>{text}</div>
             <div className="todo-actions">
                 <button className="todo-delete-button" type="button" onClick={() => setShowConfirmDelete(true)}>Delete</button>
             </div>
